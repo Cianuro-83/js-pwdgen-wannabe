bee07ed23cca969951e52c017bde5cd9bc830327
Cianuro-83 <buzzeosimone@yahoo.com>
2023-01-30 16:59:01 +0100

    FEAT: last name

--- js/app.js
+++ js/app.js
@@ -2,7 +2,16 @@ console.log(
   "prendo elemento html con id first-name e lo assegno alla sua variabile"
 );
 let name = document.getElementById("first-name");
-console.log("Chiedo il nome all'utente");
+console.log("Chiedo il Nome all'utente");
 firstName = prompt("Inserisci il tuo nome");
 console.log("imposto innerHTML di name");
 name.innerHTML = firstName;
+
+console.log(
+  "prendo elemento html con id first-name e lo assegno alla sua variabile"
+);
+let lastName = document.getElementById("last-name");
+console.log("Chiedo il Cognome all'utente");
+surname = prompt("Inserisci il tuo Cognome");
+console.log("imposto innerHTML di Last Name");
+lastName.innerHTML = surname;

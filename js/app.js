37627ad884133079065e4d9663ddda152f5c349b
Cianuro-83 <buzzeosimone@yahoo.com>
2023-01-30 16:46:12 +0100

    FEAT: richiesta nome

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-console.log("prendo elemento html con id first-name");
+console.log(
+  "prendo elemento html con id first-name e lo assegno alla sua variabile"
+);
 let name = document.getElementById("first-name");
-console.log("imposto innerHTML di name");
-name.innerHTML = name;
 console.log("Chiedo il nome all'utente");
-name = prompt("Inserisci il tuo nome");
+firstName = prompt("Inserisci il tuo nome");
+console.log("imposto innerHTML di name");
+name.innerHTML = firstName;
